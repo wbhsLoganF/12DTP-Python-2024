67a33f5c3f574752ee28628b5980986ed014a10f
wbhsLoganF <lf21549@my.westlake.school.nz>
2024-03-13 12:00:26 +1300

    e

--- blackjack_v2.py
+++ blackjack_v2.py
@@ -6,6 +6,10 @@ values = [2,3,4,5,6,7,8,9,10,"Jack","Queen","King","Ace"]
 
 
 """EMPTY CONTAINERS"""
+user_total = 0
+pc_total = 0
+user_output = ""
+pc_output = ""
 user_cards = []
 pc_cards = []
 user_values = []
@@ -21,6 +25,7 @@ for i in range (0,2):
         print("e")
     else:
         user_cards.append(user_card)
+        user_output += f"{user_card}, "
 print(user_cards)
 
 
@@ -34,10 +39,24 @@ for i in range (0,2):
         print("e")
     else:
         pc_cards.append(pc_card)
+for i in range(0,2)
 print(pc_cards)
 
 
 """USER CONVERSION"""
 for i in user_values:
     if i == "Jack" or i == "Queen" or i == "King":
-        print("e")
+        user_total += 10
+    elif i == "Ace":
+        if user_total > 11:
+            user_total += 1
+        else:
+            user_total += 11
+    else:
+        user_total += i
+
+"""DISPLAYING THE CARDS AND SCORE"""
+msg = f"You your cards are: {user_output}so your total score is {user_total}"
+easygui.msgbox(msg)
+msg = f"The dealers has the {pc_output}. What would you like to do?"
+easygui.msgbox(msg)
